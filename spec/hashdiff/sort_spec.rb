c72a52872a28419a2f8ff0e870d66905ea7f7d50
Rajat Jindal <rajatjindal83@gmail.com>
2016-10-15 20:25:59 -0700

    support for sort of object

--- spec/hashdiff/sort_spec.rb
+++ spec/hashdiff/sort_spec.rb
@@ -30,9 +30,9 @@ describe HashDiff do
   end
 
   it 'identifies difference with sorting' do
-    a = { "a": "b", "c": ["a", "c"]}
-    b = { "a": "b", "c": ["b", "a"]}
+    a = { "a": "b", "c": [{ "a": "b", "c": ["a", "b"], "d": { "a": "X", "c": ["b", "a"]}}, { "a": "b", "c": ["b", "a"], "d": { "a": "b", "c": ["a", "b"]}}], "d": { "a": "b", "c": ["b", "a"]}}
+    b = { "a": "b", "c": [{ "c": ["b", "a"], "a": "b", "d": { "a": "b", "c": ["a", "b"]}}, { "a": "b", "c": ["a", "b"], "d": { "a": "b", "c": ["b", "a"]}}], "d": { "a": "b", "c": ["a", "b"]}}
 
-    expect(HashDiff.diff(a, b, sort: true)).to eq([["-", "c[1]", "c"], ["+", "c[1]", "b"]])
+    expect(HashDiff.diff(a, b, sort: true)).to eq([["~", "c[0].d.a", "X", "b"]])
   end
 end
